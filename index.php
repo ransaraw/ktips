36d07bd852310ac39ed494a68142b9392ba93e6b
GihanTLKodikarage <gihantharaka1998@gmail.com>
2015-02-18 20:06:36 +0530

    initialization

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Ktips</title>
+		<link rel="stylesheet" href="index.css"/>
+	</head>
+	<body>
+		<header id="mainHeader"></header>
+		<div id="mainContainer"></div>
+		<footer id="mainFooter"></footer>
+	</body>
+</html>
